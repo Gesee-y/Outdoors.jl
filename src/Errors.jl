2f5991ba913bf4b67d2ba6903a132ad754f702f9
Gesee-y <gesee37@gmail.com>
2025-03-07 17:44:50 +0100

    Update Errors.jl

--- src/Errors.jl
+++ src/Errors.jl
@@ -1,6 +1,22 @@
 ################## Error codes ##############
 
+export ODError
+
+"""
+    @enum ODError begin
+        ERR_UNSUPPORTED_FEATURE
+        ERR_WINDOW_NOT_CREATED
+        ERR_CONTEX_NOT_CREATED
+    end
+
+This contain all possible problem that could be emitted by Outdoors, use it them to manage errors in your own personalized way.
+"""
 @enum ODError begin
     ERR_UNSUPPORTED_FEATURE
-    ERR_
+    ERR_WINDOW_NOT_CREATED
+    ERR_CONTEX_NOT_CREATED
 end
+
+## TODO
+# writing logs
+# Adding more descriptive error in the enumeration 
